cd5299e0e326dc33c14d36d296448afa021f4e34
Piotr Czarnecki <pioczarn@gmail.com>
2016-06-24 11:09:53 +0200

    Validate names and status enum

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,6 @@
 class Meal < ActiveRecord::Base
   belongs_to :order
   belongs_to :user
+
+  validates :name, length: { maximum: 150, too_long: "too long" }, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ActiveRecord::Base
+  enum status: [:active, :finalized, :ordered, :delivered]
+  validates :name, length: { maximum: 150, too_long: "too long" }, presence: true
 end
