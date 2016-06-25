1e9bf1e432d0b9007d409e46c2906b277e41e5c5
Piotr Czarnecki <pioczarn@gmail.com>
2016-06-26 01:13:38 +0200

    View orders with panes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,9 +14,15 @@
 //= require jquery_ujs
 //= require angular
 //= require angular-ui-router
+//= require moment/moment
+//= require angular-moment/angular-moment
+//= require bootstrap-sass-official/assets/javascripts/bootstrap
 //= require_tree .
 
-angular.module('assignment', ['ui.router'])
+// TODO cleanup: assign the module to a var
+// TODO cleanup: write 'Main as ...'?
+
+angular.module('assignment', ['ui.router', 'angularMoment'])
     .factory('orders', [
         function() {
             var svc = {
@@ -61,6 +67,20 @@ angular.module('assignment')
                     newMeal: {}
                 });
                 $scope.newOrder = {};
+            };
+            $scope.finalize = function(orderIdx) {
+                var order = $scope.orders[orderIdx];
+                $scope.ordersByState[1].push(order);
+                $scope.orders.splice(orderIdx, 1);
+            };
+            $scope.ordered = function(orderIdx) {
+                var order = $scope.orders[orderIdx];
+                order.ordered = true;
+            };
+            $scope.delivered = function(orderIdx) {
+                var order = $scope.orders[orderIdx];
+                $scope.ordersByState[2].push(order);
+                $scope.orders.splice(orderIdx, 1);
             };
             $scope.addMeal = function(order) {
                 if(!order.newMeal.name || order.newMeal.name === '') {
