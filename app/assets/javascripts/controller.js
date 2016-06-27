1db84be398bdb48817d34f2c6d57a11ef40831ee
Piotr Czarnecki <pioczarn@gmail.com>
2016-06-27 21:48:41 +0200

    Fixup split

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -1,22 +1,64 @@
 var app = angular.module('assignment');
 
-app.factory('orders', [
-    function() {
-        var svc = {
-            ordersByState: [
-                // active
-                [{
-                    name: 'x',
-                    meals: [],
-                    added: new Date(),
-                    show: true,
-                    state: 'finalized'
-                }],
-                [],
-                []
-            ]
+app.controller('Main', [
+    '$scope',
+    'orders',
+    function($scope, svc) {
+        $scope.states = ['Open', 'Archived'];
+        $scope.setTab = function(idx) {
+            if($scope.activeTab != idx) {
+                // shared reference.
+                $scope.orders = $scope.ordersByState[idx];
+            }
+            $scope.activeTab = idx;
         };
-
-        return svc;
+        $scope.newOrder = {};
+        $scope.addOrder = function() {
+            if(!$scope.newOrder.name || $scope.newOrder.name === '') {
+                return;
+            }
+            $scope.orders.push({
+                name: $scope.newOrder.name,
+                meals: [],
+                added: new Date(),
+                newMeal: {},
+                show: true,
+                state: 'finalized'
+            });
+            $scope.newOrder = {};
+        };
+        $scope.finalize = function(order) {
+            var orderIdx = $scope.orders.indexOf(order);
+            $scope.ordersByState[1].push(order);
+            $scope.orders.splice(orderIdx, 1);
+        };
+        $scope.setFinalized = function(order) {
+            order.state = 'finalized';
+        };
+        $scope.setOrdered = function(order) {
+            order.state = 'ordered';
+        };
+        $scope.setDelivered = function(order) {
+            order.state = 'delivered';
+        };
+        $scope.isFinalized = function(order) {
+            return order.state === 'finalized';
+        };
+        $scope.isOrdered = function(order) {
+            return order.state === 'ordered';
+        };
+        $scope.isDelivered = function(order) {
+            return order.state === 'delivered';
+        };
+        $scope.addMeal = function(order) {
+            if(!order.newMeal.name || order.newMeal.name === '') {
+                return;
+            }
+            order.meals.push(order.newMeal);
+            order.newMeal = {};
+        };
+        // don't clone. it's a shared reference.
+        $scope.ordersByState = svc.ordersByState;
+        $scope.setTab(0);
     }
 ]);
--- app/assets/javascripts/service.js
+++ app/assets/javascripts/service.js
@@ -1,64 +1,32 @@
 var app = angular.module('assignment');
 
-app.controller('Main', [
-    '$scope',
-    'orders',
-    function($scope, svc) {
-        $scope.states = ['Open', 'Archived'];
-        $scope.setTab = function(idx) {
-            if($scope.activeTab != idx) {
-                // shared reference.
-                $scope.orders = $scope.ordersByState[idx];
-            }
-            $scope.activeTab = idx;
+app.factory('orders', [
+	'$resource',
+	'$http',
+    function($resource, $http) {
+        //var svc = $resource('/orders', [], {"update": {"method": "PATCH"}});
+        //svc.create = function(attrs, successHandler) {
+        //};
+        var svc = {
+            ordersByState: [
+                // active
+                [{
+                    name: 'x',
+                    meals: [],
+                    added: new Date(),
+                    show: true,
+                    state: 'finalized'
+                }],
+                [],
+                []
+            ]
         };
-        $scope.newOrder = {};
-        $scope.addOrder = function() {
-            if(!$scope.newOrder.name || $scope.newOrder.name === '') {
-                return;
-            }
-            $scope.orders.push({
-                name: $scope.newOrder.name,
-                meals: [],
-                added: new Date(),
-                newMeal: {},
-                show: true,
-                state: 'finalized'
-            });
-            $scope.newOrder = {};
+        svc.getAll = function() {
+        	return $http.get('/orders').success(function(data) {
+        		angular.copy(data, svc.orders);
+        	})
         };
-        $scope.finalize = function(order) {
-            var orderIdx = $scope.orders.indexOf(order);
-            $scope.ordersByState[1].push(order);
-            $scope.orders.splice(orderIdx, 1);
-        };
-        $scope.setFinalized = function(order) {
-            order.state = 'finalized';
-        };
-        $scope.setOrdered = function(order) {
-            order.state = 'ordered';
-        };
-        $scope.setDelivered = function(order) {
-            order.state = 'delivered';
-        };
-        $scope.isFinalized = function(order) {
-            return order.state === 'finalized';
-        };
-        $scope.isOrdered = function(order) {
-            return order.state === 'ordered';
-        };
-        $scope.isDelivered = function(order) {
-            return order.state === 'delivered';
-        };
-        $scope.addMeal = function(order) {
-            if(!order.newMeal.name || order.newMeal.name === '') {
-                return;
-            }
-            order.meals.push(order.newMeal);
-            order.newMeal = {};
-        };
-        // don't clone. it's a shared reference.
-        $scope.ordersByState = svc.ordersByState;
-        $scope.setTab(0);
+
+        return svc;
     }
 ]);
