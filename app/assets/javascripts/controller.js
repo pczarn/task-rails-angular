77000787c0431099d7ce1c0f333454a8ac70e882
Piotr Czarnecki <pioczarn@gmail.com>
2016-06-28 11:37:43 +0200

    Move constants to myApp.js

--- app/assets/javascripts/controller.js
+++ app/assets/javascripts/controller.js
@@ -1,16 +1,5 @@
 var app = angular.module('assignment');
 
-app.constant('STATUSES', {
-    active: {id: 'active', name: 'Active', categoryId: 0},
-    finalized: {id: 'finalized', name: 'Finalized', categoryId: 1},
-    ordered: {id: 'ordered', name: 'Ordered', categoryId: 1},
-    delivered: {id: 'delivered', name: 'Delivered', categoryId: 1}
-});
-app.constant('CATEGORIES', [
-    {name: 'Open', defaultStatusId: 'active'},
-    {name: 'Archived', defaultStatusId: 'finalized'}
-]);
-
 app.controller('Main', [
     '$scope',
     'orders',
--- app/assets/javascripts/myApp.js
+++ app/assets/javascripts/myApp.js
@@ -1,3 +1,14 @@
 // TODO cleanup: write 'MainController as ...'?
 
 var app = angular.module('assignment', ['ui.router', 'angularMoment']);
+
+app.constant('STATUSES', {
+    active: {id: 'active', name: 'Active', categoryId: 0},
+    finalized: {id: 'finalized', name: 'Finalized', categoryId: 1},
+    ordered: {id: 'ordered', name: 'Ordered', categoryId: 1},
+    delivered: {id: 'delivered', name: 'Delivered', categoryId: 1}
+});
+app.constant('CATEGORIES', [
+    {name: 'Open', defaultStatusId: 'active'},
+    {name: 'Archived', defaultStatusId: 'finalized'}
+]);
